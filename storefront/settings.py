f2848c005e2341fe2213f88cce1b616fff84bc16
Albert <ishengomaalbert@gmail.com>
2021-09-21 08:35:09 +0300

    Initial Commit

--- storefront/settings.py
+++ storefront/settings.py
@@ -90,7 +90,12 @@ DATABASES = {
         'NAME': 'storefront',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'PASSWORD': '',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4'
+        }
     }
 }
 

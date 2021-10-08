f3328806964b6c75f59af34ba96b01a79df7ca58
Albert <ishengomaalbert@gmail.com>
2021-10-08 09:09:01 +0300

    Setup admin site & register models

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import F
+from store.models import Product, Order, OrderItem,Collection
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+  
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=7)
+    collection.save()
+
+    
+    return render(request, 'hello.html', {'name': 'AM'})
